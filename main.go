c86b810838a1ca6a99d2a263d849984245c14918
gl <gl@local>
2024-08-08 12:20:53 +0200

    Update Capitals

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 	for _, arg := range os.Args[1:] {
 		tag, err := language.Parse(arg)
 		if err != nil {
-			fmt.Printf("%s: error: %v\n", arg, err)
+			fmt.Printf("%s: Error: %v\n", arg, err)
 		} else if tag == language.Und {
-			fmt.Printf("%s: undefined\n", arg)
+			fmt.Printf("%s: Undefined\n", arg)
 		} else {
-			fmt.Printf("%s: tag %s\n", arg, tag)
+			fmt.Printf("%s: Tag %s\n", arg, tag)
 		}
 	}
 }
